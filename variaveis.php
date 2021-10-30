e8572a97598da42102beef11d7093b7b9b907687
Jeandra <76909903+Jeandra@users.noreply.github.com>
2021-10-30 20:15:52 -0300

    PHP

---
+++ variaveis.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = "Jeandra Cristina da Silva Ribeiro.";
+$idade = "33 anos";
+$raca = "negra";
+$altura = 1.57;
+$peso = 57;
+$signo = "escorpião";
+$estadocivil = "casada";
+
+echo "<p> Meu nome e $nome, tenho $idade, sou $raca do signo de $signo, $estadocivil. </p> ";
+echo"<p> Minha altura e $altura e peso $peso kg </p>";
